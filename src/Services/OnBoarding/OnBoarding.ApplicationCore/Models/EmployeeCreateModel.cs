344f65fadc43bd2c4bee0dd1d4f2406ec3379017
KarimElagamy <102981217+KarimElagamy@users.noreply.github.com>
2022-12-21 14:33:10 -0500

    Added Data Models + More Controllers
    Added Data models, fixed some entity mistakes, and added rest of controllers

---
+++ src/Services/OnBoarding/OnBoarding.ApplicationCore/Models/EmployeeCreateModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OnBoarding.ApplicationCore.Models
+{
+	public class EmployeeCreateModel
+	{
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string SSN { get; set; }
+        public DateTime HireDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public EmployeeCategoryInfoModel EmployeeCategoryCode { get; set; }
+        public EmployeeStatusInfoModel EmployeeStatusCode { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public EmployeeRoleInfoModel EmployeeRoleId { get; set; }
+    }
+}
