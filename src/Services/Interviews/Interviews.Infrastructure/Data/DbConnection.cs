cb2441a053b251a2375de093b7e06e76d6bc3e9e
Rebecca-S-Liu <87039929+Rebecca-S-Liu@users.noreply.github.com>
2022-12-20 22:41:39 -0500

    Create Database Connection

--- src/Services/Interviews/Interviews.Infrastructure/Data/DbConnection.cs
+++ src/Services/Interviews/Interviews.Infrastructure/Data/DbConnection.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 
@@ -6,13 +7,18 @@ namespace Interviews.Infrastructure.Data;
 public class DbConnection
 {
     private readonly IConfiguration _configuration;
+
     public DbConnection(IConfiguration configuration)
     {
         _configuration = configuration;
     }
+
     public SqlConnection GetConnection()
     {
-        //var conn = _configuration.GetConnectionString("HRM_Interview");
-        return new SqlConnection("Server=localhost;Database=HRM_Interview;User Id=sa;Password=Antra123!;TrustServerCertificate=true");
+        //string conn = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("HRM_Interview");
+        string conn = _configuration.GetConnectionString("HRM_Interview");
+        return new SqlConnection(conn);
     }
+
+
 }
