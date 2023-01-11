e216bdb46e1c50c4051d33072eae286590dda3e0
KarimElagamy <102981217+KarimElagamy@users.noreply.github.com>
2023-01-11 11:08:26 -0500

    dockerfile updates
    included more specific directory in all docker files

--- src/Services/Authentication/Authentication.API/obj/Debug/net6.0/Authentication.API.AssemblyInfo.cs
+++ src/Services/Authentication/Authentication.API/obj/Debug/net6.0/Authentication.API.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2d094ea4-501e-478d-97d6-defc60ee5ec3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
